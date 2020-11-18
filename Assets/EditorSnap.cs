b3aee8f9ae423767998bde855e74eda044cf3df4
Taynan Souza <taynan.tws@gmail.com>
2020-11-17 21:22:39 -0300

    Init commit
    Setting up the custom pathfinding system.
    Created the prefab cube.
    Limited the position to work as a grid (for each 10 unit)

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField] float size = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / size) * size;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / size) * size;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Realm Rush
+
+Realm Rush is a tower defense game built on Unity.
+
+## Game design
+
+![Realm Rush Game Design](./game-overview.jpg)
+
+It's a Sci Fi Tower Defense, the player will place the towers to defend and prevent damage to the base.
+
+* Enemy pathing: Enemies spawn and follow best path to reach player base.
+* Place twoers: Player can place towes that shoot at enemies within range.
+* Enemy health: Multiple hits to be killed.
