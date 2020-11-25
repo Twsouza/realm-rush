14962d909784c32e5941d6e00b1b06685c70a215
Taynan Souza <taynan.tws@gmail.com>
2020-11-25 19:31:52 -0300

    148. Revise Coroutines & Much More
    Challenge done: Spawn an enemy every X second.
    I've included a max spawn enemies.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float secondsBetweenSpawns = 5f;
+    [SerializeField] EnemyMovement enemyToSpawn;
+    [SerializeField] int maxEnemies = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        print("Max enemies: " + maxEnemies);
+        while (FindObjectsOfType<EnemyMovement>().Length < maxEnemies)
+        {
+            Object.Instantiate(enemyToSpawn);
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+    }
+}
