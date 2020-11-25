553ac6c3854c51ba8cae331dd49af7c5045b4196
Taynan Souza <taynan.tws@gmail.com>
2020-11-24 23:17:30 -0300

    146. Check For Distance
    Added healt point to the enemy.
    The Tower only shoots  if the enemy is in range.
    More blocks were added to the scene.

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +7,32 @@ public class Tower : MonoBehaviour
 {
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform targetEnemy;
+    [SerializeField] float attackRange = 30f;
+    [SerializeField] ParticleSystem projectileParticle;
 
     // Update is called once per frame
     void Update()
     {
-        objectToPan.LookAt(targetEnemy);
+        if (targetEnemy)
+        {
+            objectToPan.LookAt(targetEnemy);
+            FireAtEnemy();
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void FireAtEnemy()
+    {
+        float distanceToEnemy = Vector3.Distance(targetEnemy.transform.position, gameObject.transform.position);
+        Shoot(distanceToEnemy <= attackRange);
+    }
+
+    private void Shoot(bool v)
+    {
+        var emissionModule = projectileParticle.emission;
+        emissionModule.enabled = v;
     }
 }
