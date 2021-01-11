21aaa0be9235ce26a775eeedbadfc85ed3da39be
Taynan Souza <taynan.tws@gmail.com>
2021-01-11 14:44:20 -0300

    160. Display Health & Score
    Default health is 10, but it can be changed in the inspector.
    Score is the total enemies spawned.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -9,20 +10,32 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] EnemyMovement enemyToSpawn;
     [SerializeField] int maxEnemies = 5;
     [SerializeField] Transform enemyParentTransform;
+    [SerializeField] Text spawnedEnemies;
+
+    int score;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnEnemies());
+        spawnedEnemies.text = score.ToString();
     }
 
     IEnumerator SpawnEnemies()
     {
         while (true)
         {
+            AddScore();
+
             var enemy = Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
             enemy.transform.parent = enemyParentTransform;
             yield return new WaitForSeconds(secondsBetweenSpawns);
         }
+    }
+
+    private void AddScore()
+    {
+        score++;
+        spawnedEnemies.text = score.ToString();
     }
 }
